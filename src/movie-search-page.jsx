c7a9e826506f8fb915ed199277fea67a60f2a89d
jiabaow <jiabaowenwendy@gmail.com>
2025-01-28 11:10:45 -0800

    input + button

--- src/movie-search-page.jsx
+++ src/movie-search-page.jsx
@@ -1,3 +1,24 @@
-const MovieSearchPage = () => <div>Build your movie search page here!</div>;
+import {useState} from "react";
+
+const MovieSearchPage = () => {
+    const [input, setInput] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(input);
+    }
+
+    const handleInputChange = (e) => {
+        setInput(e.target.value);
+    }
+
+    return (
+        <div>
+            <p>Movie Search Page</p>
+            <input type="text" value={input} style={{ width: "300px"}} onChange={handleInputChange}/>
+            <button onClick={handleSubmit}>Search</button>
+        </div>
+    )
+}
 
 export default MovieSearchPage;
